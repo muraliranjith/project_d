c12e9f331142cd30da6e3390cf86ae13cc80eb43
muraliranjith <89624560+muraliranjith@users.noreply.github.com>
2022-06-10 12:09:45 +0530

    Update config.js
    feat: config file changes

--- config/config.js
+++ config/config.js
@@ -1,14 +1,29 @@
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "password",
+//     DB: "text",
+//     dialect: "mysql",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     },
+//     PORT:4040,
+//   };
+
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "password",
-    DB: "text",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    },
-    PORT:4040,
-  };
+  HOST: "206.189.137.65",
+  USER: "root",
+  PASSWORD: "PathFinder@789H",
+  DB: "text",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  PORT:3307,
+};
